44b2b844d6075b8d172626a6502c48b36cbb49f5
Vyom Pant <vypant@deloitte.com>
2022-04-05 15:12:53 +0530

    Unequal sum fixed

--- MiniAssignment1/main.py
+++ MiniAssignment1/main.py
@@ -1,48 +1,99 @@
 from itertools import permutations
 from collections import Counter
 class Stringclass:
-    def __init__(self,inpt):
-        self.string=inpt
+    string="nba"
+    def __init__(self):
+        print("Enter the string")
+        self.string = input()
+
+
     def length(self):
         return len(self.string)
     def tolist(self):
         return list(self.string)
 
 
-# class EqualSumPairs:
-#     def totalcount(self):
+class EqualSumPairs:
+    def totalcount(self):
+        self.count=permutations(list(self.string))
+        print(self.count)
 
 
 class SearchCommonElements(Stringclass):
-    def commonElements(self):
-        d=dict(Counter(list(self.string)))
-        res=[]
-        for j in d:
-            if d[j]>=2:
-                res.append(j)
-        return res
+    StringClassString = ""
+    PairPossibleString = ""
+
+    arr = []
+
+    def __init__(self, a, b):
+        self.StringClassString = a
+        self.PairPossibleString = b
+
+    def common(self):
+        dict = {}
+        for char in self.StringClassString:
+            if char in dict:
+                continue
+            else:
+                dict[char] = 1;
+        for char in self.PairPossibleString:
+            if char in dict:
+                self.arr.append(char)
+                dict.pop(char)
+
+    def printList(self):
+        print("The common elements are :: ")
+        print(self.arr)
 
 
 class Pairpossible(Stringclass):
+    arr = []
+    string="abcd"
+    def __init__(self):
+        print("Enter the second string")
+        self.string = input()
 
     def pair(self):
-        self.perm=permutations(list(self.string))
-        li=[]
-        for i in self.perm:
-            li.append(i)
-            print(list(i), end=" ")
+        for i in range(len(self.string)):
+            for j in range(i + 1, len(self.string)):
+                self.arr.append(self.string[i] + self.string[j])
     def show(self):
-        return self.li
+        print(self.arr)
 
 
-print(" Enter your number string")
-userinput=input()
-objstringclass=Stringclass(userinput)
+
+class EqualSumPairs(Pairpossible):
+    arr = Pairpossible.arr
+    def __init__(self):
+        pass
+    def countPairs(self):
+        # print(self.arr)
+        count = 0
+        dict = {}
+        for i in range(len(self.arr)):
+            sum = int(self.arr[i][0]) + int(self.arr[i][1])
+            # print(sum)
+            if str(sum) in dict:
+                dict[str(sum)] = dict[str(sum)] + 1
+            else:
+                dict[str(sum)] = 1
+        for key, val in dict.items():
+            if val == 1:
+                count = count + 1
+        return count
+
+
+objstringclass=Stringclass()
 print("Length of string is: ",objstringclass.length())
 print("String converted to list: ",objstringclass.tolist())
-objpairpossible=Pairpossible(userinput)
+objpairpossible=Pairpossible()
 print("All possible pairs are: ")
 objpairpossible.pair()
-objsearchcommonelements=SearchCommonElements(userinput)
-print()
-print("Common elements are: ",objsearchcommonelements.commonElements())
+objpairpossible.show()
+
+objsearchcommonelements=SearchCommonElements(objstringclass.string,objpairpossible.string)
+objsearchcommonelements.common()
+objsearchcommonelements.printList()
+eqsc=EqualSumPairs()
+print("unique sum count",eqsc.countPairs())
+
