135952577526367f07ee632b152b65f186b03751
Vyom Pant <vypant@deloitte.com>
2022-04-07 15:00:55 +0530

    Main Assignment , Initial commit , created menu

---
+++ MainAssignmentBookMyShow/main.py
@@ -0,0 +1,16 @@
+
+while True:
+    print("******Welcome to BookMyShow*******")
+    print("1. Login")
+    print("2. Register new Account")
+    print("3. exit")
+
+    inp = input()
+    if inp == '1':
+        print("login user method call")
+        #login user
+    elif inp == '2':
+        print("register user method call")
+        #register user
+
+    else: exit(0)
